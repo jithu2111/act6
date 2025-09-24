d1bd8003c1a8102e8aceb6f5496c13e5426545dd
sannzay <sannzayreddy@gmail.com>
2025-09-24 12:05:42 -0400

    feat: setup mission control app with custom theme and branding

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,174 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';;
+import 'package:flutter/material.dart'
+voidvoid main
+main(()) = = >
+    > runApp
+runApp((MyApp MyApp(())));;
+classMyApp
+class
+MyApp extends
+extends StatelessWidget
+StatelessWidget {
+    {
+        constMyApp
+        const
+            MyApp(({
+                {
+                    super
+                    super..key
+                    key
+                }
+            }));;
+        @override
+        @override
+        Widget build
+        Widget
+        build((BuildContext BuildContext context context)) {
+            {
+                return
+                returnMaterialApp
+                MaterialApp((
+                    title 'Mission Control', ,
+                    title::'Mission Control'
+                    debugShowCheckedModeBanner::
+                    debugShowCheckedModeBanner false, ,
+                    false theme theme::ThemeData ThemeData((
+                        useMaterial3::true useMaterial3 true, ,
+                        colorScheme::ColorScheme colorScheme ColorScheme..fromSeed fromSeed((
+                            seedColor::
+                            const
+                                seedColor
+                            const Color
+                            Color((0xFF1976D2 0xFF1976D2)), ,
+                            brightness::Brightness brightness Brightness..light light, ,
+                        )), ,
+                        appBarTheme::
+                        const
+                            appBarTheme
+                        const AppBarTheme
+                        AppBarTheme((
+                            centerTitle::true centerTitle true, ,
+                            elevation::0 0, ,
+                            elevation backgroundColor::Color backgroundColor Color((0xFF0D47A1 0xFF0D47A1)), ,
+                            foregroundColor::Colors foregroundColor Colors..whitewhite, ,
+                        )), ,
+                        elevatedButtonTheme::ElevatedButtonThemeData elevatedButtonTheme ElevatedButtonThemeData((
+                            style::ElevatedButton style ElevatedButton..styleFrom styleFrom((
+                                elevation::8 8, ,
+                                elevation padding::
+                                const
+                                    padding
+                                const EdgeInsets
+                                EdgeInsets..symmetric symmetric((horizontal horizontal::32 32, , vertical vertical::16 16)), ,
+                                shape::RoundedRectangleBorderRoundedRectangleBorder((
+                                    shape borderRadius::BorderRadius borderRadius BorderRadius..circular circular((16 16)), ,
+                                )), ,
+                            )), ,
+                        )), ,
+                    )), ,
+                    home home::
+                    const
+                    const CounterWidget
+                    CounterWidget(()), ,
+                ));;
+            }
+        }
+    }
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+class CounterWidget
+class
+CounterWidget extends
+Statef
+extends Statef
+ulWidget {
+    {
+        ulWidget
+        const CounterWidget
+        const
+            CounterWidget(({
+                {
+                    super
+                    super..key
+                    key
+                }
+            }));;
+        @override @override
+        CounterWidgetState createState
+        CounterWidgetState
+        createState(()) = = >
+            > CounterWidgetState
+        CounterWidgetState(());;
+    }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+class CounterWidgetState
+class
+CounterWidgetState extends
+extends State
+State < < CounterWidget
+CounterWidget > > {
+    {
+        int _counter = = 0 0;;
+        int _counter
+        @override
+        @override
+        Widget build
+        Widget
+        build((BuildContext BuildContext context context)) {
+            {
+                return
+                return Scaffold
+                Scaffold((
+                    backgroundColor::Colors backgroundColor Colors..grey grey[[50 50]], ,
+                    appBar::AppBar appBar AppBar((
+                        title::Row title Row((
+                            mainAxisAlignment::MainAxisAlignmentMainAxisAlignment..center mainAxisAlignment center, ,
+                            children::[
+                                [
+                                    children
+                                    Icon((Icons Icon Icons..rocket_launch rocket_launch, , color color::Colors Colors..whitewhite)), ,
+                                    const SizedBox
+                                    const
+                                        SizedBox((widthwidth::8 8)), ,
+                                        const Text
+                                    const
+                                        Text(('MISSION CONTR
+                                            'MISSION CONTR
+                                            OL ',, style
+                                            OL '
+                                            style::TextStyle TextStyle((fontWeight fontWeight::FontWeight FontWeight..bold bold)))), ,
+                                ]
+                            ], ,
+                        )), ,
+                        flexibleSpace::Container flexibleSpace Container((
+                            decoration::
+                            const
+                                decoration
+                            const BoxDecoration
+                            BoxDecoration((
+                                gradient::LinearGradientLinearGradient((
+                                    gradient begin::Alignment begin Alignment..centerLeft centerLeft, ,
+                                    end::Alignment end Alignment..centerRight centerRight, ,
+                                    colors::[
+                                        [Color
+                                            colors
+                                            Color((0xFF0D47A1 0xFF0D47A1)), , Color
+                                            Color((0xFF1976D2 0xFF1976D2))
+                                        ]
+                                    ], ,
+                                )), ,
+                            )), ,
+                        )), ,
+                    )), ,
+                    body::Center body Center((
+                        child::Text child Text((
+                            'Counter: $ $_counter
+                            'Counter:
+                            _counter '', ,
+                            style::Theme style Theme..of of ((context context))..textTheme textTheme..headlineMedium headlineMedium, ,
+                        )), ,
+                    )), ,
+                ));;
+            }
+        }
+    }
 }
